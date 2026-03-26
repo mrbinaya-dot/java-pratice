4311e1c84ea52d8e921d1cca4c8a22745b4a96e0
mrbinay-dot <nrnay166@gmail.com>
2026-03-26 15:53:18 +0545

    completed

--- src/Threads/Company.java
+++ src/Threads/Company.java
@@ -1,15 +1,38 @@
+
 package Threads;
 
 public class Company {
-      int n;
-     synchronized public void produce_items(int n) {
+
+    int n;
+    boolean f = false;
+
+    synchronized public void produce_items(int n) {
+
+        if (f) {
+            try {
+                wait();
+            } catch (Exception e) {
+            }
+        }
+
         this.n = n;
-        System.out.println("produced : "+ this.n);
+        System.out.println("Produced : " + this.n);
+        f = true;
+        notify();
     }
-    synchronized public int consume_item(){
-        System.out.println("Consumed :" + this.n);
+
+    synchronized public int consume_item() {
+
+        if (!f) {
+            try {
+                wait();
+            } catch (Exception e) {
+            }
+        }
+
+        System.out.println("Consumed : " + this.n);
+        f = false;
+        notify();
         return this.n;
     }
-
-
 }
